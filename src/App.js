68096d00530af07927c0bbe1428f8a8c2f7dc984
Maraton <despaircaptain99@gmail.com>
2024-03-13 18:34:49 +0300

    главная страница

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-
+import './style.css';
+import logo from './ФОТОЧКИ/лого_круг.png';
 function App() {
   return (
-    <div className="">
+    <div className="Header">
+      <p>О нас</p>
+      <p>Контакты</p>     
+      <p>Меню</p>
+      <p>Сезонные предложения</p>
+      <div className='Logo'>
+        <img src={logo} alt='Кофе_сердце' />
+      </div>
     </div>
+
   );
 }
 
