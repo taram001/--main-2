5ab42817eae9b100c33f338eb4e899adb2475266
Maraton <despaircaptain99@gmail.com>
2024-03-17 02:21:01 +0300

    переименовал файлы

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
-import './style.css';
+import './launding_page.css';
 import logo from './ФОТОЧКИ/лого_круг.png';
-function App() {
+function App()
+ {	
   return (
     <div className="Header">
-      <p>О нас</p>
-      <p>Контакты</p>     
+      <p className='TextWithUnderline' >О нас</p>
+      <p className='TextWithUnderline'>Контакты</p>     
       <div className='Logo'>
         <img src={logo} alt='Кофе_сердце' />
         <p className='Name_kafe'>Любовь в ритме кофе</p> 
       </div>
-      <p>Меню</p>
-      <p>Сезонные предложения</p>
+      <p className='TextWithUnderline'>Меню</p>
+      <p className='TextWithUnderline'>Сезонные предложения</p>
     </div>
 
   );
